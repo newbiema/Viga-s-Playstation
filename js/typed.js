e3435d6b48f87704437b5950a092f82089bf0416
Evan Jamaq <xevan19@gmail.com>
2025-05-18 02:43:19 +0700

    update

---
+++ js/typed.js
@@ -0,0 +1,16 @@
+  document.addEventListener("DOMContentLoaded", function () {
+    new Typed("#typed-text", {
+      strings: [
+        "Main Bareng di Tempat Seru!",
+        "Main PS3 & PS4 Sepuasnya!",
+        "Suasana Cozy & Game Lengkap!",
+        "Viga's Playstation 🔥"
+      ],
+      typeSpeed: 60,
+      backSpeed: 40,
+      backDelay: 2000,
+      loop: true,
+      showCursor: true,
+      cursorChar: '|',
+    });
+  });
